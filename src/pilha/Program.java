14ab332b51767c6afdabf0780f04b3fb6a1fe153
ErickFernandes-Algnologia <erick.farias@dcx.ufpb.br>
2021-03-24 13:23:31 -0300

    Project: Stack in java

---
+++ src/pilha/Program.java
@@ -0,0 +1,34 @@
+package pilha;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		 
+		StackArray<String> sa = new StackArray<String>(3);
+		
+		System.out.println("Lista de nomes: ");
+		
+		sa.push("Erick");
+		sa.push("Jose");
+		sa.push("Maria");
+		sa.push("Gabriel");
+			
+		System.out.println(sa);
+
+		sa.pop();
+		
+		System.out.println(sa);
+		
+		sa.pop();
+		
+		System.out.println(sa);
+		
+		sa.pop();
+		
+		System.out.println(sa);
+		
+		
+		
+	}
+
+}
